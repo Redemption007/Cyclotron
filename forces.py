376326878bc487836c0e88b472ba47e5e89234d5
Redemption007 <74996732+Redemption007@users.noreply.github.com>
2022-02-25 12:52:57 +0100

    Create forces.py

---
+++ forces.py
@@ -0,0 +1,17 @@
+#Fichier de définition des forces, pour l'application de la deuxième loi de Newton
+def laplace() :
+  '''
+  On définit ici l'intensité de la force de Laplace dans les dés à l'aide de plusieurs paramètres :
+  - La position qui servira à savoir si la particule se trouve dans un dé ou non.
+  - la vitesse, qui sera une variable au cours de la simulation
+  - l'intensité du champ, qui pourra être modifiée en entrée utilisateur.
+  - la masse de la particule, pouvant être modifiée selon des préréglages donnés (électron, proton, neutron, atomes)
+  La fonction retournera l'unique force qui s'applique sur la particule.
+  '''
+  return null
+def champ() :
+  '''
+  Selon les mêmes paramètres, on définira ici la force du champ électrique appliqué à l'interstice entre les dés conducteurs.
+  La fonction retournera l'unique force s'appliquant sur la particule.
+  ''''
+  return null
