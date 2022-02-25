85862de693e5fee3cefbaade4449ce1ef2f7240c
Redemption007 <74996732+Redemption007@users.noreply.github.com>
2022-02-25 16:52:49 +0100

    Create variations.py

---
+++ variations.py
@@ -0,0 +1,25 @@
+from tkinter import * as tk
+'''
+L'idée est de disposer de boutons réglables avec la souris à côté de la modélisation, pour voir l'influence des différents paramètres sur le système.
+Il y aura ainsi soint un continuum de valeurs possibles, soit seulement certaines valeurs prédéfinies (dans le cas de la masse par exemple).
+Au total, 6 paramètres seront variables, et ils resteront figés pour toute la simulation dès que celle-ci sera lancée.
+Il faudra alors arrêter la modélisation et la reprendre de 0 pour mettre à jours les réglages voulus.
+'''
+def btn_phase():
+  #bouton réglant la phase de l'inversion de la tension.
+  return
+def amplitude():
+  #bouton réglant le maximum et le minimum de tension atteints.
+  return
+def champ():
+  #bouton réglant l'intensité du champ électromagnétique dans les dés.
+  return
+def potentiel():
+  #bouton réglant le potentiel des dés.
+  return
+def vitesse initiale():
+  #bouton réglant la vitesse initiale de la particule.
+  return
+def masse():
+  #bouton réglant la masse de la particule.
+  return
