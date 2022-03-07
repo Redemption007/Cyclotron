7ebf4934d9f6490fb10d75e0d53fc7f519e22897
Fabinouuuu <99190265+Fabinouuuu@users.noreply.github.com>
2022-03-07 16:07:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #déclaration des variables
+import forces
 B(Bx=0,By=0,Bz=1,5T) #champ magnétique
 E(Ex=0,Ey=5·103V,Ez=0) #champ électrique
 m = 1,67·10−27 #masse de la particule (kg)
 q = 1,6·10−19 #quantité de charge de la particule (C)
 
 #Initialisation des variables
-E = 
+E =
+'''
+forces.laplace et force.champ sont les fonctions appelées dans la seconde loi de Newton
+'''
