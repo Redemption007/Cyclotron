6e358a4d89b3167dfc3da4c38d9bc2c50fabd18b
Fabinouuuu <99190265+Fabinouuuu@users.noreply.github.com>
2022-03-07 16:33:17 +0100

    Update forces.py

--- forces.py
+++ forces.py
@@ -1,8 +1,21 @@
-#Fichier de définition des forces, pour l'application de la deuxième loi de Newton
+#
+"""
+Aide du module forces :
+
+NOM :
+    forces - Fichier de définition des forces, appelées pour l'application de la deuxième loi de Newton.
+
+FONCTIONS :
+    laplace(x_1, x_2, position_x, v, Bz, m, q)
+        Retourne la force de Laplace, calculée avec Bz, q et v, en fonction de la position_x de la particule. Définition propre du rayon de courbure R, calculée avec v, m, q et Bz.
+
+    champ(x_1, x_2, position_x, Ex, q, U)
+        Retourne la force du champ, calculée avec Bz, q et v, en fonction de la position_x de la particule. 
+"""
 def laplace(x_1, x_2, position_x, v, Bz, m, q) :
   R = (v*m)/(q*Bz) #rayon de courbure
   
-  '''
+  """
   On définit ici l'intensité de la force de Laplace dans les dés à l'aide de plusieurs paramètres :
   - La position qui servira à savoir si la particule se trouve dans un dé ou non (x doit être entre x_1 et x_2 compris).
   - la vitesse, qui sera une variable au cours de la simulation
@@ -10,7 +23,7 @@ def laplace(x_1, x_2, position_x, v, Bz, m, q) :
   - la masse de la particule, pouvant être modifiée selon des préréglages donnés (électron, proton, neutron, atomes)
   - la charge de la particule, en multiples de e.
   La fonction retournera l'unique force qui s'applique sur la particule.
-  '''
+  """
   return null
 
 def champ(x_1, x_2, position_x, Ex, q, U) :
@@ -19,8 +32,8 @@ def champ(x_1, x_2, position_x, Ex, q, U) :
   F = float(Ex * q * U)                   #Sinon, on retourne la force exercée par le champ, multipliée par la tension pour accélerer la particule dans le bon sens.
   print("F = ", F) #Log de vérification du code. A ENLEVER A LA FIN
   return F
-  '''
+  """
   Selon les mêmes paramètres, ainsi que la tension (qui permettra de savoir dans quel sens est accélérée la particule), 
   on définira ici la force du champ électrique appliqué à l'interstice entre les dés conducteurs.
   La fonction retournera l'unique force s'appliquant sur la particule.
-  ''''
+  """
