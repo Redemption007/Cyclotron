6cea3f6f0b94671222fd18a79c018706d85e9818
Redemption007 <74996732+Redemption007@users.noreply.github.com>
2022-03-07 15:54:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+#déclaration des variables
+B(Bx=0,By=0,Bz=1,5T) #champ magnétique
+E(Ex=0,Ey=5·103V,Ez=0) #champ électrique
+m = 1,67·10−27 #masse de la particule (kg)
+q = 1,6·10−19 #quantité de charge de la particule (C)
 
+#Initialisation des variables
+E = 
