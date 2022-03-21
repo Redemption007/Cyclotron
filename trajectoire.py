56d20003b09845f352c5cb1f22b58abf08df54d6
Redemption007 <74996732+Redemption007@users.noreply.github.com>
2022-03-21 15:45:17 +0100

    Update and rename Trajectoire.py to trajectoire.py

--- trajectoire.py
+++ trajectoire.py
@@ -2,7 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
 
-def trajectoire (R, v, m, B, q) :
+def newton(equation_x, equation_y) :
+  a_x = sp.Poly(equation_x/m, t)
+  a_y = sp.Poly(equation_y/m, t)
+  v_x = sp.Poly(a_x.integrate()+vx_init, t)
+  v_y = sp.Poly(a_y.integrate()+vy_init, t)
+  x = v_x.integrate()+x_init
+  y = v_y.integrate()+y_init
+  norme = np.sqrt(v_x**2+x_y**2)
+  
   '''
   On rentre une fonction dedans, qui correspondra à l'accélération subie par la particule. On intègrera numériquement pour en ressortir la vitesse
   (qui sera affichée en sortie écran utilisateur dans un tableau) ainsi que la trajectoire, qui sera modélisée en sortie écran utilisateur dans un graphe.
@@ -10,7 +18,7 @@ def trajectoire (R, v, m, B, q) :
   Ainsi les précédents points seront au fur et à mesure supprimés pour garder une vitesse correcte et ne pas créer de latence dans la simulation.
   '''
   
-  return null
+  return x, y, norme
 
 '''
 Ici, un exemple de trajectoire circulaire pour pouvoir superposer les fonctions dans un premier temps et vérifier les intégrations des zones 2 et 4 (dans les dés)
